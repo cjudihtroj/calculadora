4e35956b8e9a99966d9c6356e69648a01ceceaf6
cjudihtroj <168495292+cjudihtroj@users.noreply.github.com>
2024-09-18 23:56:04 -0500

    Update Resta.java

--- src/main/Resta.java
+++ src/main/Resta.java
@@ -1,10 +1,21 @@
 package main.negocio.calculo;
 
 public class Resta implements Aritmetica {
-	
-	@Override
-	public double calcular(double primerTermino, double segundoTermino) {
-		return primerTermino - segundoTermino;
-	}
+
+    /**
+     * Realiza la sustracción del segundo término del primer término proporcionado.
+     * Este método calcula la diferencia entre dos números dados y retorna el resultado.
+     * No hay validaciones específicas para los valores de entrada ya que la sustracción
+     * en Java maneja adecuadamente los rangos de valores normales, incluyendo cero y valores negativos.
+     * Sin embargo, es importante tener en cuenta la posibilidad de resultados negativos, especialmente
+     * en contextos donde se esperan valores no negativos.
+     * @param primerTermino el minuendo en la sustracción.
+     * @param segundoTermino el sustraendo en la sustracción.
+     * @return el resultado de sustraer el segundo término del primer término.
+     */
+    @Override
+    public double calcular(double primerTermino, double segundoTermino) {
+        return primerTermino - segundoTermino;
+    }
 
 }
