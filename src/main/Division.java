91e59a40edda6d66e8f60684a342b8712199cb42
cjudihtroj <168495292+cjudihtroj@users.noreply.github.com>
2024-09-18 23:51:32 -0500

    Update Division.java

--- src/main/Division.java
+++ src/main/Division.java
@@ -1,13 +1,23 @@
 package main.negocio.calculo;
 
 public class Division implements Aritmetica {
-	
-	@Override
-	public double calcular(double primerTermino, double segundoTermino) {
-		if(segundoTermino == 0) {
-			throw new IllegalArgumentException("No se puede dividir por cero");
-		}
-		
-		return primerTermino/segundoTermino;
-	}
+    
+    /**
+     * Realiza la división de dos términos proporcionados.
+     * Valida que el segundo término no sea cero para evitar la división por cero.
+     * Si el segundo término es cero, lanza una IllegalArgumentException.
+     * Este método es responsable de asegurar que la división se realice correctamente sin errores aritméticos.
+     * @param primerTermino el numerador en la división.
+     * @param segundoTermino el denominador en la división; no debe ser cero.
+     * @return el resultado de dividir el primer término por el segundo término.
+     * @throws IllegalArgumentException si el segundo término es cero, para prevenir la división por cero.
+     */
+    @Override
+    public double calcular(double primerTermino, double segundoTermino) {
+        if (segundoTermino == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        
+        return primerTermino / segundoTermino;
+    }
 }
