67b24e02119e4c1d2445e0a26582d0b1f5e0a728
cjudihtroj <168495292+cjudihtroj@users.noreply.github.com>
2024-09-18 23:54:52 -0500

    Update Producto.java

--- src/main/Producto.java
+++ src/main/Producto.java
@@ -2,9 +2,19 @@ package main.negocio.calculo;
 
 public class Producto implements Aritmetica {
 
-	@Override
-	public double calcular(double primerTermino, double segundoTermino) {
-		return primerTermino * segundoTermino;
-	}
+    /**
+     * Realiza la multiplicación de dos términos proporcionados.
+     * Este método calcula el producto de dos números dados y retorna el resultado.
+     * No hay validaciones específicas para los valores de entrada ya que la multiplicación
+     * en Java maneja adecuadamente los rangos de valores normales, incluyendo cero y valores negativos.
+     * Sin embargo, se debe considerar el manejo de la posible sobrefluencia (overflow) para valores extremadamente grandes.
+     * @param primerTermino el primer factor en la multiplicación.
+     * @param segundoTermino el segundo factor en la multiplicación.
+     * @return el resultado de multiplicar el primer término por el segundo término.
+     */
+    @Override
+    public double calcular(double primerTermino, double segundoTermino) {
+        return primerTermino * segundoTermino;
+    }
 
 }
