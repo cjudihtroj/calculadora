457125b18a369537c6d3f806a6b246d00eaeeb48
cjudihtroj <168495292+cjudihtroj@users.noreply.github.com>
2024-09-18 23:57:14 -0500

    Update Suma.java

--- src/main/Suma.java
+++ src/main/Suma.java
@@ -2,9 +2,19 @@ package main.negocio.calculo;
 
 public class Suma implements Aritmetica {
 
-	@Override
-	public double calcular(double primerTermino, double segundoTermino) {
-		return primerTermino + segundoTermino;
-	}
-	
+    /**
+     * Realiza la suma de dos términos proporcionados.
+     * Este método calcula la suma de dos números dados y retorna el resultado.
+     * No hay validaciones específicas para los valores de entrada ya que la suma
+     * en Java maneja adecuadamente los rangos de valores normales, incluyendo cero y valores negativos.
+     * Sin embargo, se debe considerar el manejo de la posible sobrefluencia (overflow) para valores extremadamente grandes.
+     * @param primerTermino el primer sumando en la operación de suma.
+     * @param segundoTermino el segundo sumando en la operación de suma.
+     * @return el resultado de sumar el primer término al segundo término.
+     */
+    @Override
+    public double calcular(double primerTermino, double segundoTermino) {
+        return primerTermino + segundoTermino;
+    }
+    
 }
